16a383cfee2e09ae020d670534abcd0aa46e3fa3
henicosa <ludwig.david.lorenz@uni-weimar.de>
2024-08-14 13:58:47 +0200

    add cache test

--- edit.py
+++ edit.py
@@ -1,5 +1,30 @@
 import os
 from datetime import datetime
+import json
+
+# create new cache directory if it does not exist
+if not os.path.exists('cache'):
+    os.makedirs('cache')
+
+# create new counter.json file if it does not exist
+if not os.path.exists('cache/counter.json'):
+    with open('cache/counter.json', 'w') as file:
+        file.write('{"count": 0}')
+
+# load counted executions from cache/counter.json
+with open('cache/counter.json', 'r') as file:
+    data = json.load(file)
+
+# Increment the count from key 'count'
+count = data['count'] + 1
+
+# Update the count in the data
+data['count'] = count
+
+# Write the updated data back to the file
+with open('cache/counter.json', 'w') as file:
+    json.dump(data, file)
+
 
 # Define the public directory and the index file path
 public_dir = 'public'
@@ -16,9 +41,10 @@ if os.path.exists(index_file):
 else:
     content = '<html><body><p>{{time}}</p></body></html>'  # Default content if index.html does not exist
 
+
 # Replace {{time}} with the current time
 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-new_content = content.replace('{{time}}', current_time)
+new_content = content.replace(current_time + " and was executed " + str(count) + " times") 
 
 # Write the new content back to index.html
 with open(index_file, 'w') as file:
